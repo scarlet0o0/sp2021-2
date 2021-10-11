83d5618fc7dbdc4810023fda7518977d492e7423
scarlet0o0 <ghd7673@naver.com>
2021-10-11 07:55:02 -0400

    문제 4번과 마지막 문제 풀이

--- w06/p4/measure_time.c
+++ w06/p4/measure_time.c
@@ -3,14 +3,16 @@
 #include <limits.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+
 //#define CLK_TCK ((clock_t)_SC_CLK_TCK)
 
 void func_01(int *arr,int s);
 void func_ON(int *arr,int s);
 void func_ON2(int *arr,int s);
 int main(int argc,char* argv[]){
-	double CLK_TCK = ((clock_t)_SC_CLK_TCK);
-	int size = 40000;
+	//double CLK_TCK = ((clock_t)_SC_CLK_TCK);
+	int size = 59999;
 	int arr[size];
 	for (int i = 0 ; i<size ; i++){
 		arr[i] = i;
@@ -20,7 +22,6 @@ int main(int argc,char* argv[]){
 	struct tms mytms;
 	clock_t t1_1,t1_2,t2_1,t2_2,t3_1,t3_2;
 	
-	printf("clk tck : %.1f  \n",CLK_TCK);
 	if ((t1_1 = times(&mytms)) == -1){ 
 		perror("times1_1"); 
 		exit(1);
@@ -30,7 +31,6 @@ int main(int argc,char* argv[]){
 		perror("times1_1"); 
 		exit(1);
 	}
-	printf("t1: %.1f \n",(double)(t1_2-t1_1));
 	if ((t2_1 = times(&mytms)) == -1){ 
 		perror("times1_1"); 
 		exit(1);
@@ -40,7 +40,7 @@ int main(int argc,char* argv[]){
 		perror("times1_1"); 
 		exit(1);
 	}
-	printf("2\n");
+	//printf("2\n");
 	if ((t3_1 = times(&mytms)) == -1){ 
 		perror("times1_1"); 
 		exit(1);
@@ -50,10 +50,10 @@ int main(int argc,char* argv[]){
 		perror("times1_1"); 
 		exit(1);
 	}
-	printf("3\n");
-	printf("func_01 takes %.1f seconds.\n",(double)(t1_2-t1_1)/CLK_TCK);
-	printf("func_ON takes %.1f seconds.\n",(double)(t2_2-t2_1)/CLK_TCK);
-	printf("func_ON2 takes %.1f seconds.\n",(double)(t3_2-t3_1)/100);
+	//printf("3\n");
+	printf("func_01 takes %.1f seconds.\n",((double)(t1_2-t1_1)/CLOCKS_PER_SEC)*10000);
+	printf("func_ON takes %.1f seconds.\n",((double)(t2_2-t2_1)/CLOCKS_PER_SEC)*10000);
+	printf("func_ON2 takes %.1f seconds.\n",((double)(t3_2-t3_1)/CLOCKS_PER_SEC)*10000);
 	return 0;
 }
 
